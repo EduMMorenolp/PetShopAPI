0a9ede0fa20b0bb2068f21cf9219cf784061f11e
Edu Moreno <e.m.morenolp@gmail.com>
2024-06-14 23:40:27 -0300

    error de instalacion npm uninstall bcrypt

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const homeRouter = require('./routes/home.routes')
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./routes/user.routes');
 
 const app = express()
 const PORT = process.env.PORT || 3000
