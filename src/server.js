565a194da07385069b23e94807abfbea51c9fc1e
Edu Moreno <e.m.morenolp@gmail.com>
2024-06-19 01:11:06 -0300

    implementacion de cors para trabajar localmente

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const homeRouter = require('./routes/home.routes')
 const userRoutes = require('./routes/user.routes');
+const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express()
@@ -10,6 +11,10 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 // Morgan para registrar todas las solicitudes HTTP
 app.use(morgan('dev'));
+// Cors para permitir solicitudes desde cualquier origen
+app.use(cors());
+// Para manejar las solicitudes OPTIONS
+app.options('*', cors());
 
 // Ruta home
 app.use('/', homeRouter)
